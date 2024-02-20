861aa770b08dcad4d478bd590316fd423218f1ce
Manikandan-Dharma <157537135+Manikandan-Dharma@users.noreply.github.com>
2024-02-20 15:04:33 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_instance" "SERVER-TOMCAT" {
+  ami           = "ami-0e731c8a588258d0d"   
+  instance_type = "t2.micro" 
+  key_name = "FINAL KEYPAIR"
+  monitoring = true
+
+  tags = {
+    Name = "SERVER-TOMCAT"
+  }
+}
