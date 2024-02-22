084495b385fd16b6d58195e21ab40e429c1f89b6
Manikandan-Dharma <manikandan.dharma@outlook.com>
2024-02-22 05:53:59 -0500

    m

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1" 
 }
 
-resource "aws_instance" "SERVER1" {
+resource "aws_instance" "TESTSERVER1" {
   ami           = "ami-0e731c8a588258d0d"   
   instance_type = "t2.micro" 
   key_name = "FINAL KEYPAIR"
   monitoring = true
 
   tags = {
-    Name = "SERVER1"
+    Name = "TESTSERVER1"
   }
 }
